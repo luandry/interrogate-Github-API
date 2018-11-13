85476f30496efa80fb6a0d9b0d579085409f2980
Luan Williams <willialu@tcd.ie>
2018-11-13 14:22:58 +0000

    Vanity checker, compares Followers vs Following and displays data in pie chart

--- importRequests.py
+++ importRequests.py
@@ -1,6 +1,7 @@
 import requests 
 import json 
 import sys
+import matplotlib.pyplot as plt
 
 userName = sys.argv[1]
 
@@ -13,8 +14,29 @@ data = r.json()
 followers = data["followers"]
 following = data["following"]
 
-print('Followers: ' ,followers)
-print('following: ', following)
+# Data to plot
+labels = 'Vain(Followers)', 'Humble(Following)'
+sizes = [followers, following]
+colors = ['gold', 'lightskyblue']
+
+plt.pie(sizes, labels=labels, colors=colors,
+        autopct='%1.1f%%', startangle=140)
+ 
+plt.axis('equal')
+plt.title('Vanity Checker, checks Followers vs Following')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+#print('Followers: ' ,followers)
+#print('following: ', following)
 
 
 
